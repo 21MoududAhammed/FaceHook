9c017b16a02bb3b5862a64f61b24a64359fe557c
21MoududAhammed <21moudud@gmail.com>
2024-04-18 11:02:59 +0600

    Routing has done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import {Routes, Route} from 'react-router-dom'
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import NotFoundPage from './pages/NotFoundPage';
+
 export default function App(){
     return (
-        <h1>Hello Bangladesh!</h1>
+        <Routes>
+          <Route path="/" element={<HomePage/>} exact/>
+          <Route path="/register" element={<RegisterPage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/me" element={<ProfilePage/>}/>
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
     );
 }
