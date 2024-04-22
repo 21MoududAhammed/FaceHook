c3afd34798126c050a9a76b714f2a7fb6844e3dc
21MoududAhammed <21moudud@gmail.com>
2024-04-22 12:36:15 +0600

    Created a useAxios hook where we handles the jwt token

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,60 @@
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import api from "../api/api";
+import axios from "axios";
+
+const useAxios = () => {
+  const { auth, setAuth } = useAuth();
+  useEffect(() => {
+    const authToken = auth?.authToken;
+    // request intercept
+    const requestIntercept = api.interceptors.request.use(
+      (config) => {
+        if (authToken) {
+          config.headers["Authorization"] = `Bearer ${authToken}`;
+        }
+        return config;
+      },
+      (err) => Promise.reject(err)
+    );
+    // response intercept
+    const responseIntercept = api.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      async (err) => {
+        const originalRequest = err.config;
+        // if the error status is 401 and there is no originalRequest._retry flag, it means the token has expired and we need to refresh it.
+        if(err.response.status === 401 && !originalRequest._retry){
+            originalRequest._retry = true;
+            const refreshToken = auth?.refreshToken;
+            try{
+                const response = await axios.post(`${import.meta.env.VITE_BASE_SERVER_URL}/auth/refresh-token`, {refreshToken});
+                const {token} = response?.data;
+                // set the new token as authToken 
+                setAuth({...auth, authToken: token});
+
+                //retry the original request with the new token 
+                originalRequest.headers.Authorization = `Bearer ${token}`;
+                return axios(originalRequest);
+                
+            }catch(err){
+                throw err;
+            }
+           
+        }
+        return Promise.reject(err);
+      }
+      
+    );
+
+    // clean up function
+    return () => {
+      api.interceptors.request.eject(requestIntercept);
+      api.interceptors.response.eject(responseIntercept);
+    };
+  }, [auth?.authToken]);
+  return { api };
+};
+
+export default useAxios;
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,23 @@
-export default function ProfilePage(){
-    return (
-        <h1>Profile</h1>
-    );
+import { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
+import useAxios from "../hooks/useAxios";
+
+
+export default function ProfilePage() {
+    const [user, setUser] = useState(null);
+  const { auth } = useAuth();
+  const { api } = useAxios();
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await api.get(`/profile/${auth?.user?.id}`);
+        setUser(response.data.user);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchProfile();
+  }, []);
+  return <h1>Name: {user?.firstName} {user?.lastName}</h1>;
 }
