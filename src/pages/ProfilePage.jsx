c2640a5c575b660e6bede03720e56a09252755a8
21MoududAhammed <21moudud@gmail.com>
2024-04-23 12:42:57 +0600

    Loading and error state added to the ProfilePage

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,22 +2,43 @@ import { useEffect, useState } from "react";
 import useAuth from "../hooks/useAuth";
 import useAxios from "../hooks/useAxios";
 
-
 export default function ProfilePage() {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  
   const { auth } = useAuth();
   const { api } = useAxios();
 
   useEffect(() => {
     const fetchProfile = async () => {
+      setLoading(true);
       try {
         const response = await api.get(`/profile/${auth?.user?.id}`);
-        setUser(response.data.user);
+        setUser(response?.data?.user);
+        setPosts(response?.data?.posts);
       } catch (err) {
         console.log(err);
+        setError(err);
+      } finally {
+        setLoading(false);
       }
     };
     fetchProfile();
   }, []);
-  return <h1>Name: {user?.firstName} {user?.lastName}</h1>;
+
+
+  if(loading){
+    return <div>Profile data is fetching ......</div>
+  }
+
+  return (
+    <div>
+      <h1>
+        Name: {user?.firstName} {user?.lastName}
+      </h1>
+      <h2>posts: {posts?.length}</h2>
+    </div>
+  );
 }
