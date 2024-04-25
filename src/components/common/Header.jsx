8ab12e84e25fec73e972268a5fa0cd3c2c754565
21MoududAhammed <21moudud@gmail.com>
2024-04-25 15:43:12 +0600

    profile picture sync with header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,10 +5,14 @@ import avatar from '../../assets/images/avatars/avatar_1.png'
 import Logout from './Logout';
 import {Link} from 'react-router-dom'
 import useAuth from '../../hooks/useAuth'
+import useProfile from '../../hooks/useProfile'
 
 
 export default function Header() {
   const {auth} = useAuth();
+  const {state} = useProfile();
+
+  const user = state?.user ?? auth?.user;
   
   return (
     <nav className="sticky top-0 z-50 border-b border-[#3F3F3F] bg-[#1E1F24] py-4">
@@ -31,11 +35,11 @@ export default function Header() {
           </button>
            <Logout/>
           <button className="flex-center !ml-8 gap-3">
-            <span className="text-lg font-medium lg:text-xl">{auth?.user?.firstName}</span>
+            <span className="text-lg font-medium lg:text-xl">{user?.firstName}</span>
             <img
-              className="max-h-[32px] max-w-[32px] lg:max-h-[44px] lg:max-w-[44px]"
-              src={avatar}
-              alt=""
+              className="h-8 w-8 rounded-full lg:max-h-[44px] lg:max-w-[44px]"
+              src={`${import.meta.env.VITE_BASE_SERVER_URL}/${user?.avatar}`}
+              alt="avatar"
             />
           </button>
         </div>
