256de429d997edf2f6e322719d1fd221116bf13f
21MoududAhammed <21moudud@gmail.com>
2024-04-25 20:23:54 +0600

    All posts fetched and  displayed on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,39 @@
-import Header from "../components/common/Header";
+import useAxios from "../hooks/useAxios";
 import NewPost from "../components/home/NewPost";
-import useAuth from "../hooks/useAuth";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import PostList from "../components/posts/PostList";
 
 export default function HomePage() {
-  const { auth } = useAuth();
+  const { api } = useAxios();
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        setLoading(true);
+        const response = await api.get("/posts");
+        if (response?.status === 200) {
+          setPosts(response.data);
+        }
+      } catch (err) {
+        console.log(err);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  if (loading) return <div>Loading.....</div>;
+  if (error) return <div>{error}</div>;
+
   return (
     <div>
-      <NewPost/>
+      <NewPost />
+      <PostList posts={posts} />
     </div>
   );
 }
